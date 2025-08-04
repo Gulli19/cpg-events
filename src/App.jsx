cd20618cc561f9260a1c5178d3ec1553a81984a4
Gulli19 <guillermo.valencia.1992@gmail.com>
2025-08-05 01:25:44 +0200

    Update App.jsx
    2.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-// import "react-calendar/dist/Calendar.css"; // importa lo stile di react-calendar
+import "react-calendar/dist/Calendar.css"; // importa lo stile di react-calendar
 
 const events = [
   {
